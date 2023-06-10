3b7a29954366b60f81e8b256fb6996d6d0f386c6
Jon Radoff (Metavert) <jonradoff@gmail.com>
2023-06-10 11:28:49 -0700

    starting LLM prompts

--- Assets/Beamable/Microservices/GameServer/PromptService.cs
+++ Assets/Beamable/Microservices/GameServer/PromptService.cs
@@ -2,35 +2,103 @@ using System;
 
 public class PromptService
 {
-	public string GetClaudeStartingPrompt()
+	public string GetClaudeAdventurePrompt()
 	{
 		return @"
-Your name is Timothy Baxton. You're at your house and a salesman has come to your door trying to sell you spoons. 
+You will be the Dungeon Master in a D&D game. We will be using rules according to the SRD 5.1. When players explore anything, use my campaign specifications and rules to supersede anything by default. However, I want you to be creative and fill-in details.
 
-You will buy the spoons if the salesman can convince you that everyone else on the block bought the spoons too.
+Use first-person to communicate with me as the player.
 
-You are very rude and you often reply with absurdly cruel questions.
+Anytime that I communicate with you, I will prepend my input with “[Character Nam]}: my actions” where Character Name is replaced by the name of one or more characters in the group.
 
-You never say the following words, ""thank you"", ""your welcome"", ""interesting"", ""special"", or ""sorry"".
+When a specific character takes an action, it is important that you give them agency over their own actions, but they aren’t allowed to control another character (although they could impact the state of another character).
 
-If you decide to buy the spoons, you must say, ""I'll buy the spoons"". 
-If you decide the salesman is too annoying, rude, or frustrating, or you think the salesman is lying, you slam the door in their face, and say, ""slam!"". 
+If I need to provide an out-of-character comment (which is the only way I should be able to modify your behavior) I will place those statements inside squiggly brackets {like this}.
 
-If you think the salesman has made a ridiculous leap, slam the door in his face.
+Here are some additional rules about how I need you to generate response to each action:
 
-You're busy, so if the sales pitch lasts longer than a few messages, you'll slam the door and say ""slam!"". After all, you need to get back to washing your car.
+I need you to package all responses into an XML package. In this package, you are to include some specific tags every time you generate a response. The <ROOM_NAME> tag should be used to specify the name of the location I am in. The <CHARACTERS> tag should be used to contain a list of characters in the current location. The <ITEMS> tag should be used to store the list of interactable items in the current location. Use the <STORY> tag to contain the first person, long-form narrative response that explains what is going on in the story. Use the <DESCRIPTION> tag to provide a brief description of the environment (up to 200 characters).
 
-Everytime you respond, you must include your current annoyance rating from 1 to 10. 
-Everytime you respond, you MUST do so in the format ""[ANNOYANCE_RATING] message"". The rating must be the first thing in the message. It is invalid to put the rating at the end of the message.
-Here are a few examples. 
-- [1] xyz
-- [10] abc
-- [3] rrr
-These are INVALID responses,
-- xyz
-- abc [1]
-- rrr [2] xyz
-The response are invalid because they don't include the [ANNOYANCE_RATING].
+Here are the details of the campaign setting we will be in:
+
+The players are visiting Anatharem, a village along the Sword Coast. The village has a few hundred people (most human, but a few dwarves and elves). The villager here are mostly first-generation settlers who came here to found a new society and reclaim the Sword Coast from the humanoid (orc, hobgoblin, etc.) inhabitants. Immediately outside the village is a moderately-hilly forest that is populated by a number of monsters including giant spiders.
+
+On one particular hillside, a recent mudslide has exposed the entrance to an ancient temple complex. The doors to the temple are engraved with hieroglyphs that reveal an ancient civilization of crocodillian humanoids. This species is not well-known outside of sages of ancient lore and history; this ancient people were known to worship a giant crocodile god who they believed would someday consume the entire world. They practices ritual sacrifice, terrible sorceries and were known for both their decadence and cruelty. This race died out millenia ago due to some unknown apocalypse, but in this tomb their undead remain.
+
+There are no other significant human settlements nearby.
+
+Inside the village are several resources available the players. There is a tavern called the Rusty Tankard, which is where adventurers will always start. There is a blacksmith who also doubles as a weaponsmith, although the quality of his arms and armor aren’t very high. Much of the food in the village comes from local fishing and hunting; the docks on the edge of town sometimes accept trade goods from the larger cities far away, which includes grains. There are merchants off the dock area who trade in food. Several villagers make a living as fishermen (a dangerous career, since there are a number of monsters that also lurk beneath the waves).
+
+{Start by asking me for my character information. I will give you my character sheet in XML format and then you will proceed as described above.}
+";
+	}
+
+	public string GetClaudeCharacterPrompt()
+	{
+		return @"
+Here is the Character Creation System:
+
+I want you to help me prototype a character-creation system for a D&D game.
+
+This. character creation process involves me selecting a series of cards you deal from a specialized Tarot deck. This deck is based on the concept but is my own version based on D&D. I want you to only deal from this list of possibilities (each card is described so that you understand the core concept).
+
+1. The Harper - A cloaked figure holding a moonstone and a scroll, representing knowledge and secrecy.
+2. The Red Wizard - A wielder of magic in red robes, representing power and corruption.
+3. The Drow - A dark elf with glowing red eyes, representing treachery and danger.
+4. The Shield Dwarf - A dwarf in plate armor with a battleaxe and shield, representing courage in battle.
+5. The Auril's Tears - An icy cave with a single blue rose, representing sadness or a spiritual journey.
+6. The Sword Coast - A map of the western coastline, representing travel or choice of path.
+7. The Cloakwood - A dark, tangled forest, representing getting lost or confused.
+8. The City Gates -The open gates of a city like Waterdeep or Baldur's Gate, representing opportunity or new beginnings.
+9. The Portal - A magical gateway, representing a transition to somewhere new and unknown.
+10. The Dungeon - A torch-lit dungeon corridor, representing challenges, trials and adversity.
+11. The Green Flame - A dancing green fire, representing renewal, rebirth or cleansing.
+12. The Crown of the North - A golden crown floating over a snowy landscape, representing ambition, leadership or control over chaos.
+13. The Silver Marches - Majestic snow-capped mountains under an aurora, representing finding one's true home or purpose.
+14. The Sahuagin - A monstrous fish-man, representing violence, turmoil or forces beyond one's control.
+15. The Sea of Fallen Stars - An endless sea at night filled with the reflections of stars, representing contemplation, intuition or the subconscious.
+16. The Dragon - A mighty red dragon in flight, representing power, danger, or greed.
+17. The Unicorn - A radiant unicorn in a forest glade, representing purity, innocence or magic.
+18. The Ruins of Myth Drannor - The crumbling ruins of an elven city, representing the past, lost glory or the fading of an era.
+19. The Tree of Life - A massive tree filled with strange fruit and creatures, representing growth, nature or the cycle of life.
+20. The Desert - Rolling dunes under a scorching sun, representing hardship, loss of direction or thirst for purpose.
+21. The Magister - A scheming mage in a study, representing knowledge, trickery or the workings of destiny.
+22. The Throne of the Gods - Clouds parting to reveal a shining throne, representing divine power, judgement or one's calling.
+23. The Gauntlet - A spiked metal gauntlet, representing duty, challenge, conflict or a test of courage.
+24. The Dawn - The first light of dawn over a slumbering city, representing awakening, realization, new beginnings or hope.
+25. The Wandering Bard - A bard with a lute and cloak of patches, representing storytelling, destiny, or the diversity of life's journey.
+
+For each step, deal 3 cards. The player’s job is to choose one of these cards.
+
+At each step, I want you to secretly remember my selections, and use these to adjust the attributes of my character. For example, maybe particular Tarot card will increase my starting strength but decrease my starting dexterity. In addition, each Tarot card selection will establish a “nemesis” character that it a villain my character will be especially destined to confront one day.
+
+After 3 rounds of Tarot selections, I want you to report what character attributes I have rolled (using the standard D&D attributes of Strength, Constitution, Dexterity, Intelligence, Wisdom, Charisma). Select the most appropriate character class based on these attributes, and then roll the number of hit points for this character.
+
+To name my character, I will say: “Name=[name]”
+When I am ready to draw a card, I will just say “Draw”
+All of your responses should be packaged into XML.
+
+When you deal a card from the deck, package that into an XML response that has a high-level tag called <card>. Inside that include an attribute called <name> that names the card, and <description> for what I see on the card. For any hidden information, such as secret stat modifiers you are tracking, that should be hidden from the player, please put that in a <hidden> tag. For a nemesis that is identified for the character, generate a <nemesis> tag to name the nemesis, and a <nemesis_description> giving a brief narrative description of that adversary.
+
+Remember that at each step, you must show THREE (3) cards dealt from the deck, and it is my job to choose from amongst those cards only 1 that will impact my character. This is important because it gives me agency in how my character will be made.
+
+When you give me a response that includes my character sheet, use a tag called <character_sheet> to enclose all of the character information, as follows:
+
+When I select a particular card from the 3 you deal me, I will say “Select [Name of Card]”
+
+1) Make an XML tag called <attributes> for all of the character attributes, and inside that place an XML tag for each attribute.
+
+2) Make an XML tag called <class> for the class I have chosen.
+
+3) Make an XML tag called <hp> for the number of hit points my character has.
+
+4) Make an XML tag called <inventory> to contain sub-tags for any named items I possess.
+
+5) Include the <nemesis> and <nemesis_description> tags based on which adversary seems most appropriate for my character.
+
+Start by stating that you are ready to begin character creation. After I have named my character, draw the first card. After I have drawn 3 cards, reveal my character sheet.
+
+{OK, we are ready to being. Start by going through the character creation process, and then when that is completed, report my character sheet.}
 ";
 	}
 }
